edf1846b8311e35a1accae45cef5630381a8f44f
Evan Truesdale <kayakcat@truecrew.net>
2021-10-11 10:59:04 -0700

    Figured out plotting parameters, fixed sawtooth width

--- plot.py
+++ plot.py
@@ -2,9 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Section III.B plotting
-data_reference = np.loadtxt('avg_rho_0.dat')
-for size in np.arange(5, 85, 5):
-    data = np.loadtxt('avg_rho_{_size}.dat'.format(_size=int(size)))
-    plt.plot(data[:,0], data[:,1] - data_reference[:,1])
-    plt.savefig('screening_rho_{_size}.png'.format(_size=int(size)), dpi=300)
-    plt.clf()
+#data_reference = np.loadtxt('avg_BaTiO3_7_Ba-O_P4mm_reference.dat')
+#for size in np.arange(5, 85, 5):
+#    data = np.loadtxt('rho_BaTiO3_7_Ba-O_P4mm_{_size}.dat'.format(_size=int(size)))
+#    plt.plot(data[:,0], data[:,1] - data_reference[:,1])
+#    plt.savefig('screening_rho_BaTiO3_7_Ba-O_P4mm_{_size}.png'.format(_size=int(size)), dpi=300)
+#    plt.clf()
+
+# Section III.A plotting
+data = np.loadtxt('charge_density_BaTiO3_7_Ba-O_P4mm_0.dat')
+plt.plot(data[:,0], data[:,1])
+plt.savefig('charge_density_BaTiO3_7_Ba-O_P4mm_0.png', dpi=300)
+plt.clf()
+
+data = np.loadtxt('ionic_hartree_potential_BaTiO3_7_Ba-O_P4mm_0.dat')
+plt.plot(data[:,0], data[:,1])
+plt.savefig('ionic_hartree_potential_BaTiO3_7_Ba-O_P4mm_0.png', dpi=300)
+plt.clf()
+
+data = np.loadtxt('ionic_potential_BaTiO3_7_Ba-O_P4mm_0.dat')
+plt.plot(data[:,0], data[:,1])
+plt.savefig('ionic_potential_BaTiO3_7_Ba-O_P4mm_0.png', dpi=300)
+plt.clf()
+
+data = np.loadtxt('sawtooth_BaTiO3_7_Ba-O_P4mm_0.dat')
+plt.plot(data[:,0], data[:,1])
+plt.savefig('sawtooth_BaTiO3_7_Ba-O_P4mm_0.png', dpi=300)
+plt.clf()
+
+data = np.loadtxt('total_potential_BaTiO3_7_Ba-O_P4mm_0.dat')
+plt.plot(data[:,0], data[:,1])
+plt.savefig('total_potential_BaTiO3_7_Ba-O_P4mm_0.png', dpi=300)
+plt.clf()
